63ad9ea4ead0a6085ae23c793e00a148d3892123
ofrikas <ofrikas1@gmail.com>
2024-07-03 23:30:07 +0300

    done edit comment
    feat: Add edit functionality to CommentCard component

--- src/Components/PlayVideo/CommentCard.js
+++ src/Components/PlayVideo/CommentCard.js
@@ -1,13 +1,51 @@
-import React from 'react'
-import { parseUploadTime } from '../Feed/VideoCard'
+import React, { useState } from 'react';
+import { parseUploadTime } from '../Feed/VideoCard';
 import { openDB } from 'idb';
 
 export const CommentCard = ({ comment, index, loggedInUser, videoId }) => {
-    // Placeholder functions for handling edit and delete actions
-    const handleEditComment = () => {
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [editedCommentText, setEditedCommentText] = useState(comment.commentText);
 
-        // Implement edit logic here
+    const handleEditComment = () => {
+        setIsEditMode(!isEditMode);
     };
+
+    const handleSaveEditedComment = async () => { 
+        const db = await openDB('MeaTubeDB');
+        if (!db.objectStoreNames.contains('videos')) {
+            console.error("Object store 'videos' does not exist in commentcaed handle Delete.");
+            return;
+        }
+        const tx = db.transaction('videos', 'readwrite');
+        const store = tx.objectStore('videos');
+        const allVideos = await store.getAll();
+        const video = allVideos.find(v => v.id === Number(videoId));
+        if (!video) {
+            console.error('Video not found.');
+            return;
+        }
+        video.commentsLink = video.commentsLink.map(c => {
+            if (c.id === comment.id) {
+                c.commentText = editedCommentText;
+            }
+            return c;
+        }
+        );
+        await store.put(video);
+        await tx.done;
+        console.log('Comment edited and saved successfully.');
+        setIsEditMode(false);
+    };
+
+    const handleLike = async () => {
+        alert('like botton is not implemented yet');
+    };
+
+    const handleDislike = async () => {
+        alert('dislike botton is not implemented yet');
+    };
+
+
 
     const handleDeleteComment = async () => {
         const db = await openDB('MeaTubeDB');
@@ -24,7 +62,6 @@ export const CommentCard = ({ comment, index, loggedInUser, videoId }) => {
             return;
         }
         video.comments--;
-        
         console.log('Comment:', comment);
         video.commentsLink = video.commentsLink.filter(c => c.id !== comment.id);
         console.log('Video:', video);
@@ -36,15 +73,23 @@ export const CommentCard = ({ comment, index, loggedInUser, videoId }) => {
     return (
         <div className='comment' key={index}>
             <img src={comment.userImage} alt='commenter' className="commenter-image" />
-            <div>
+            <div >
                 <h3>{comment.displayName} <span>{parseUploadTime(comment.timestamp)}</span></h3>
-                <p>{comment.commentText}</p>
+                { isEditMode ? (
+                    <>
+                    <div className='add-comment-container'>
+                        <input value={editedCommentText} onChange={(e) => setEditedCommentText(e.target.value)} className="add-comment-input"/>
+                        <button className="add-comment-button" onClick={handleSaveEditedComment}>Done</button>
+                        </div>
+                    </>
+                ) : (
+                    <p>{comment.commentText}</p>
+                )}
                 <div className='comment-action'>
-                    <i className="bi bi-hand-thumbs-up-fill"></i>
-                    <span>{comment.likesNum}</span>
-                    <i className="bi bi-hand-thumbs-down-fill"></i>
-                    <span>{comment.dislikesNum}</span>
-                    {/* Conditional rendering based on whether the logged-in user is the comment author */}
+                    <i className="bi bi-hand-thumbs-up-fill" onClick={handleLike}></i>
+                    <span>0</span>
+                    <i className="bi bi-hand-thumbs-down-fill" onClick={handleDislike}></i>
+                    <span>0</span>
                     {loggedInUser === comment.userName && (
                         <>
                             <i className="bi bi-pencil-square" onClick={handleEditComment} style={{ cursor: 'pointer' }}></i>
